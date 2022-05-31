aadb4cf6904a5e6feab599ed2be0fc786c178e9a
Matheus Barbosa Pereira <matheus.pereira@fortbrasil.com.br>
2022-05-31 16:58:12 -0300

    upando app

---
+++ src/components/Form/ResultImc/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, TouchableOpacity, Share } from "react-native";
+import styles from "./style";
+
+
+export default function ResultImc(props) {
+
+    const onShare = async () => {
+        const result = await Share.share({
+            message: "Meu imc hoje é: " + props.resultImc
+        })
+    }
+    return (
+        <View style={styles.contextImc}>
+            <View style={styles.boxShareButton}>
+                <Text style={styles.information}>{props.messageResultImc}</Text>
+                <Text style={styles.resultImc}>{props.resultImc}</Text>
+                <TouchableOpacity
+                    onPress={onShare}
+                    style={styles.share}
+                >
+                    <Text style={styles.shareText}>Compartilhar</Text>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    )
+}
